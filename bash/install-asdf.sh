ed29011fd1a573a3a8f1cdc4c49b47f78d3009a4
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-11-09 13:40:54 +0900

    fix: support ARCH

--- bash/install-asdf.sh
+++ bash/install-asdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -qq -y --no-install-recommends git ca-certificates \
+apt-get install -qq -y --no-install-recommends git \
   && git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v${ASDF_VERSION} \
   && echo 'legacy_version_file = yes' > $HOME/.asdfrc
 
--- bash/install-litestream.sh
+++ bash/install-litestream.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apt-get install -qq -y wget \
-  && wget --quiet https://github.com/benbjohnson/litestream/releases/download/v${LITESTREAM_VERSION}/litestream-v${LITESTREAM_VERSION}-linux-amd64.deb \
+  && wget --quiet https://github.com/benbjohnson/litestream/releases/download/v${LITESTREAM_VERSION}/litestream-v${LITESTREAM_VERSION}-linux-${ARCH}.deb \
   && dpkg-reconfigure debconf -f noninteractive -p critical \
-  && dpkg -i litestream-v${LITESTREAM_VERSION}-linux-amd64.deb \
+  && dpkg -i litestream-v${LITESTREAM_VERSION}-linux-${ARCH}.deb \
   && litestream version \
-  && rm -f litestream-v${LITESTREAM_VERSION}-linux-amd64.deb
+  && rm -f litestream-v${LITESTREAM_VERSION}-linux-${ARCH}.deb
