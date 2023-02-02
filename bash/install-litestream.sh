775dfcd1a63628a277c4d2f5402b9bd80e7f612a
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2023-02-02 14:22:34 +0900

    Update install-litestream.sh

--- bash/install-litestream.sh
+++ bash/install-litestream.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LITESTREAM_VERSION=0.3.9
+
 apt-get install -qq -y wget \
   && wget --quiet https://github.com/benbjohnson/litestream/releases/download/v${LITESTREAM_VERSION}/litestream-v${LITESTREAM_VERSION}-linux-${ARCH}.deb \
   && dpkg-reconfigure debconf -f noninteractive -p critical \
