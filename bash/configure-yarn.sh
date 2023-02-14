342989ba21a5c28650b8756eb06af5e75de7cc19
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-02-14 22:02:25 +0900

    feat: add configure-yarn.sh

---
+++ bash/configure-yarn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yarn config set enableTelemetry 0
+yarn config set enableGlobalCache 0
+yarn config set nmMode hardlinks-local
+yarn config set logFilters --json {"code":"YN0007","level":"discard"}
+yarn config set logFilters --json {"code":"YN0013","level":"discard"}
+yarn config set logFilters --json {"code":"YN0019","level":"discard"}
+yarn plugin remove plugin-auto-install
--- bash/install-asdf.sh
+++ bash/install-asdf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ASDF_VERSION=0.11.1
+
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v${ASDF_VERSION} \
   && echo 'legacy_version_file = yes' > $HOME/.asdfrc
-
