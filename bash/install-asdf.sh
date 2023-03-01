8df0765ee644a79ef02750d494204dfe63238d0f
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2023-03-01 21:45:55 +0900

    Update install-asdf.sh

--- bash/install-asdf.sh
+++ bash/install-asdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ASDF_VERSION=0.11.1
+ASDF_VERSION=0.11.2
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v${ASDF_VERSION} \
   && echo 'legacy_version_file = yes' > $HOME/.asdfrc
