8d65ca2a86014200355eda6b4efb7457253ef315
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2023-04-30 09:06:33 +0900

    fix: refine configure-yarn.sh

--- bash/configure-yarn.sh
+++ bash/configure-yarn.sh
@@ -4,4 +4,4 @@ yarn config set enableTelemetry 0
 yarn config set enableGlobalCache 0
 yarn config set nmMode hardlinks-local
 yarn config set logFilters --json '[{"code":"YN0007","level":"discard"},{"code":"YN0013","level":"discard"},{"code":"YN0019","level":"discard"}]'
-yarn plugin remove plugin-auto-install || true
+yarn plugin remove plugin-auto-install &> /dev/null || true
