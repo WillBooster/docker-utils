03f3584955a018608f7bb6515cc65f9e1ba2ec78
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-05-01 00:00:53 +0900

    fix: re-add prepare-asdf.sh

--- bash/install-asdf.sh
+++ bash/install-asdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -qq -y --no-install-recommends git \
-  && ASDF_VERSION=$(curl --silent "https://api.github.com/repos/asdf-vm/asdf/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') \
+# This script does not require superuser privileges; however, the prepare-asdf.sh does require them.
+ASDF_VERSION=$(curl --silent "https://api.github.com/repos/asdf-vm/asdf/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') \
   && git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch ${ASDF_VERSION} \
   && echo 'legacy_version_file = yes' > $HOME/.asdfrc
---
+++ bash/prepare-asdf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt-get install -qq -y --no-install-recommends git
