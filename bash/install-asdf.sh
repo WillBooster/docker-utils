a770242bc3eb3619b3a0e115f41667a5cf58b371
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-08-02 17:29:24 +0900

    fix: prefer curl over wget

--- bash/install-asdf.sh
+++ bash/install-asdf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This script does not require superuser privileges; however, the prepare-asdf.sh does require them.
-ASDF_VERSION=$(curl --silent "https://api.github.com/repos/asdf-vm/asdf/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') \
+apt-get -qq install -y --no-install-recommends git curl \
+  && ASDF_VERSION=$(curl --silent "https://api.github.com/repos/asdf-vm/asdf/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') \
   && git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch ${ASDF_VERSION} \
   && echo 'legacy_version_file = yes' > $HOME/.asdfrc
--- bash/install-litestream.sh
+++ bash/install-litestream.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt-get -qq install -y --no-install-recommends git wget \
+apt-get -qq install -y --no-install-recommends curl \
   && LITESTREAM_VERSION=$(curl --silent "https://api.github.com/repos/benbjohnson/litestream/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') \
-  && wget --quiet https://github.com/benbjohnson/litestream/releases/download/${LITESTREAM_VERSION}/litestream-${LITESTREAM_VERSION}-linux-${ARCH}.deb \
+  && curl -sLO https://github.com/benbjohnson/litestream/releases/download/${LITESTREAM_VERSION}/litestream-${LITESTREAM_VERSION}-linux-${ARCH}.deb \
   && dpkg-reconfigure debconf -f noninteractive -p critical \
   && dpkg -i litestream-${LITESTREAM_VERSION}-linux-${ARCH}.deb \
   && litestream version \
