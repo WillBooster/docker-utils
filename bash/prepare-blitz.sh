8802df0cd04545b8a8d492068130b01d75fa81ee
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-08-03 16:47:32 +0900

    feat: add prepare-node-web.sh

--- bash/prepare-blitz.sh
+++ bash/prepare-blitz.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Blitz.js requires libssl-dev. wb requires procps.
+# TODO: merge this with bash/prepare-node-web.sh
 apt-get -qq install -y --no-install-recommends ca-certificates libssl-dev procps tzdata
