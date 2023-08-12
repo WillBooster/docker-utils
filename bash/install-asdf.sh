0f5d3c584bc43bc7d7c40f1f8ddcf10d03fd688e
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-08-12 18:28:25 +0900

    docs: refine comment

--- bash/install-asdf.sh
+++ bash/install-asdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script does not require superuser privileges; however, the prepare-asdf.sh does require them.
+# This script MUST NOT require superuser privileges; however, the prepare-asdf.sh does require them.
 ASDF_VERSION=$(curl --silent "https://api.github.com/repos/asdf-vm/asdf/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') \
   && git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch ${ASDF_VERSION} \
   && echo 'legacy_version_file = yes' > $HOME/.asdfrc
