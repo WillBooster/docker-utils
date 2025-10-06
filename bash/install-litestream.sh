d62ca00d0b5d58e434185a0025071952c4b73fad
Sakamoto, Kazunori <exkazuu@gmail.com>
2025-10-06 17:12:22 +0900

    fix: fix bash/install-litestream.sh

--- bash/install-litestream.sh
+++ bash/install-litestream.sh
@@ -2,7 +2,7 @@
 
 apt-get -qq install -y --no-install-recommends curl \
   && LITESTREAM_VERSION=$(curl --silent "https://api.github.com/repos/benbjohnson/litestream/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/^v//') \
-  && curl -sLO https://github.com/benbjohnson/litestream/releases/download/${LITESTREAM_VERSION}/litestream-${LITESTREAM_VERSION}-linux-${ARCH}.deb \
+  && curl -sLO https://github.com/benbjohnson/litestream/releases/download/v${LITESTREAM_VERSION}/litestream-${LITESTREAM_VERSION}-linux-${ARCH}.deb \
   && dpkg-reconfigure debconf -f noninteractive -p critical \
   && dpkg -i litestream-${LITESTREAM_VERSION}-linux-${ARCH}.deb \
   && litestream version \
