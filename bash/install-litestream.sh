3883dc207ad0e99b91b4a18f385d2c3a12f685d6
Sakamoto, Kazunori <exkazuu@gmail.com>
2025-12-12 15:42:46 +0900

    fix: update bash/install-litestream.sh

--- bash/install-litestream.sh
+++ bash/install-litestream.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Force to use 0.5.2 since 0.5.3 is broken (cannot upload backup to S3)
 apt-get -qq install -y --no-install-recommends curl \
   && LITESTREAM_VERSION=$(curl --silent "https://api.github.com/repos/benbjohnson/litestream/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/^v//') \
   && LITESTREAM_VERSION=${LITESTREAM_VERSION:-0.5.2} \
-  && LITESTREAM_VERSION=0.5.2 \     # Force to use 0.5.2 since 0.5.3 is broken (cannot upload backup to S3)
+  && LITESTREAM_VERSION=0.5.2 \
   && DEB_FILE="litestream-${LITESTREAM_VERSION}-linux-${ARCH}.deb" \
   && echo "Installing Litestream: ${DEB_FILE}" \
   && curl -sLO https://github.com/benbjohnson/litestream/releases/download/v${LITESTREAM_VERSION}/${DEB_FILE} \
